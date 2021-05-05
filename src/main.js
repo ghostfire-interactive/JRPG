57efcff66245db908cc4e6ab72a865c48cd4febb
Timothy O'Brien <ghostfire.interactive@gmail.com>
2021-05-05 09:30:41 -0400

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import TitleScene from './scenes/TitleScene';
+import WorldScene from './scenes/WorldScene';
+import BootScene from './scenes/BootScene';
+import LoadingScene from './scenes/LoadingScene';
+
+let titleScene = new TitleScene();
+let worldScene = new WorldScene();
+let bootScene = new BootScene();
+let loadingScene = new LoadingScene();
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0}
+        }
+    }
+};
+
+let game = new Phaser.Game(config);
+game.scene.add('TitleScene', titleScene);
+game.scene.add('WorldScene', worldScene);
+game.scene.add('BootScene', bootScene);
+game.scene.add('LoadingScene', loadingScene);
+game.scene.start('BootScene', {scene: 'title'});
